6ff656809c245025d028ccca2a6beb1edb3f3a37
Huascar <huascarmc@hotmail.com>
2017-09-27 09:52:35 +0100

    Added improvements + function

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,35 +1,36 @@
+#Return 10
 def return_10
   return 10
 end
-
-def add(num1, num2)
-  return num1 + num2
+#Add
+def add(num_1, num_2)
+  return num_1 + num_2
 end
-
-def subtract(num1, num2)
-  return num1 - num2
+#Subtract
+def subtract(num_1, num_2)
+  return num_1 - num_2
 end
-
-def multiply(num1, num2)
-  return num1 * num2
+#
+def multiply(num_1, num_2)
+  return num_1 * num_2
 end
-
-def divide(num1, num2)
-  return num1 / num2
+#
+def divide(num_1, num_2)
+  return num_1 / num_2
 end
-
-def length_of_string(string)
-  return string.length
+#
+def length_of_string(input_string)
+  return input_string.length()
 end
-
-def join_string(string1, string2)
-  return string1 + string2
+#
+def join_string(string_1, string_2)
+  return string_1 + string_2
 end
-
-def add_string_as_number(string1, string2)
-  return string1.to_i + string2.to_i
+#
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i() + string_2.to_i()
 end
-
+#
 def number_to_full_month_name(number)
   case number
   when 1
@@ -40,7 +41,7 @@ def number_to_full_month_name(number)
     return "September"
   end
 end
-
+#
 def number_to_short_month_name(number)
   case number
   when 1
@@ -52,14 +53,22 @@ def number_to_short_month_name(number)
   end
 end
 
+#Refactored last function to call full_month function and slice it's results to characters.
+
+def number_to_short_month_name(number)
+  full_month_name = number_to_full_month_name(number)
+  return full_month_name.slice(0, 3)
+end
+
+#
 def volc(number)
   return number**3
 end
-
+#
 def vols(number)
   return (4/3)*3.14*(number**3)
 end
-
+#
 def temperature(number)
   return ((number - 32) / 1.8).round(3)
 end
